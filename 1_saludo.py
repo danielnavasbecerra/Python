25e17d6932d5a18bd8ca5bbb5bf62c36eb1b9996
danielnavasbecera <danielnavasb@gmail.com>
2023-12-14 17:35:12 -0500

    renombrados:     saludo.py -> 1_saludo.py 	renombrados:     circulos.py -> 2_circulos.py 	nuevos archivos: 3_promedio.py 	borrados:        cond_ejemplos.py

--- 1_saludo.py
+++ 1_saludo.py
@@ -1,5 +1,5 @@
 # Escriba un programa que pida al usuario que escriba su nombre, y lo
 # salude llamándolo por su nombre.
 
-nombre = input("Ingrese su nombre: ")
+nombre = str(input("Ingrese su nombre: "))
 print("Hola, ", nombre, "!")
---
+++ 3_promedio.py
@@ -0,0 +1,10 @@
+# Escriba un programa que calcule el promedio de 4 notas
+# ingresadas por el usuario:
+
+n1 = int(input("Primera nota: "))
+n2 = int(input("Segunda nota: "))
+n3 = int(input("Tercera nota: "))
+n4 = int(input("Cuarta nota: "))
+
+oper = (n1 + n2 + n3 + n4) / 4
+prom = print("El promedio es: ", round(oper, 2))
