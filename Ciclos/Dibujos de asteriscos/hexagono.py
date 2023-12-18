27ff41ff9dddfb21ff086dadaadeb0f7709feed5
danielnavasbecerra <danielnavasb14@gmail.com>
2023-12-18 15:30:43 -0500

    nuevos archivos: Ciclos/Dibujos de asteriscos/hexagono.py 	nuevos archivos: Ciclos/Dibujos de asteriscos/triangulo.py

---
+++ Ciclos/Dibujos de asteriscos/hexagono.py
@@ -0,0 +1,2 @@
+# Escriba un programa que dibuje el hexágono del tamaño
+# indicado por el usuario de acuerdo al ejemplo:
---
+++ Ciclos/Dibujos de asteriscos/triangulo.py
@@ -0,0 +1,2 @@
+# Escriba un programa que dibuje el triángulo del
+# tamaño indicado por el usuario de acuerdo al ejemplo:
